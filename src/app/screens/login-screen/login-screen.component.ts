a6e5ab2c174a7567e9cabeb5c63b8ee6f2092c5b
Angel Pineda <lamostace@outlook.com>
2023-09-15 11:30:19 -0600

    Updated login screen
    Added "view password" button.

--- src/app/screens/login-screen/login-screen.component.ts
+++ src/app/screens/login-screen/login-screen.component.ts
@@ -1,15 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 declare var $: any;
 @Component({
   selector: 'app-login-screen',
   templateUrl: './login-screen.component.html',
   styleUrls: ['./login-screen.component.scss']
 })
-export class LoginScreenComponent {
+export class LoginScreenComponent implements OnInit{
 
   type: String = 'password';
+  visible: Boolean = false;
+  changetype: boolean = true;
 
+  viewPass(){
+    this.visible = !this.visible;
+    this.changetype = !this.changetype;
+  }
+
+  
   constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  
 
 
 }
